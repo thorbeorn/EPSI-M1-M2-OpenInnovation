30fe4f7e67babf2ba086894e2f6ea147b40f30cf
Thorbeorn <dylanllodrapro@gmail.com>
2026-03-02 17:41:41 +0100

    new electron app client

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface Window {
-  electron: {
-    send: (channel: string, data?: any) => void;
-    once: (channel: string, callback: (...args: any[]) => void) => void;
-    on: (channel: string, callback: (...args: any[]) => void) => (() => void) | undefined;
-    invoke: (channel: string, data?: any) => Promise<any>;
-  }
-}
